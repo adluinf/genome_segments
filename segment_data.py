8a62c2d9112e66eb31e97fa49052ba7fdb60ba02
Adriaan <57450752+adluinf@users.noreply.github.com>
2021-11-24 19:16:55 +0100

    Fixed a bug in Segment and implemented test for overlap.

--- segment_data.py
+++ segment_data.py
@@ -16,17 +16,32 @@ from numpy import loadtxt, fromstring
 
 class SegmentData:
 
+    def __init__(self, input_string, use_string=False):
+        """Docstring for __init__.
+
+        :input_string: string data: either a path to a file, or data to be
+        loaded.
+        :use_string: default False, if True the input_string is to be parsed as data.
+
+        """
+        if use_string:
+            self.load_string(input_string)
+        else:
+            self.load_file(input_string)
+
     def load_file(self,file_name):
         """
             This method loads a segment from a file.
         """
-        #print('TODO')
         self.data=loadtxt(file_path)
 
 
     def load_string(self, input_string):
         """
             This method loads a segment from a string.
+
+            :input_string: string containing data, one segment per line,
+            values separated by tabs.
         """
         data=[]
         for line in input_string.split('\n'):
--- tests.py
+++ tests.py
@@ -12,12 +12,11 @@ class TestGenomeMethods(unittest.TestCase):
     #def test_upper(self):
     #    self.assertEqual('foo'.upper(), 'FOO')
 
-
     def test_load_segment(self):
         example_segment_string="""1	2
 	    3	6"""
-        example_segment_data=SegmentData()
-        example_segment_data.load_string(example_segment_string)
+        example_segment_data=SegmentData(example_segment_string, use_string=True)
+        #example_segment_data.load_string(example_segment_string)
         # Check that at least one segment is loaded.
         self.assertTrue(len(example_segment_data.data) > 1)
         # Check that we have two segments.
@@ -44,38 +43,56 @@ class TestGenomeMethods(unittest.TestCase):
         self.assertEqual(len(example_function_data.data),7)
 
     def test_overlap(self):
-        estimated_overlap=0
+        segments_string_1=""" 1 2
+                            3 6"""
+        segments_string_2="""  0 1
+                            1 5"""
+        segments_1=SegmentData(segments_string_1, use_string=True)
+        segments_2=SegmentData(segments_string_2, use_string=True)
+
+        estimated_overlap=overlap(segments_1.data, segments_2.data)
         self.assertEqual(estimated_overlap,3)
 
     def test_correlation(self):
-        example_function_string_1="""10.0
+        function_string_1="""10.0
 11.0
 12.0
 13.0
 14.0
 15.0
 16.0"""
-        example_function_string_2="""10.5
+        function_string_2="""10.5
 11.5
 12.0
 13.0
 13.5
 15.0
 14.0"""
-        example_functions_1=FunctionData(example_function_string_1, use_string=True)
-        example_functions_2=FunctionData(example_function_string_2, use_string=True)
-        estimated_correlation=correlation(example_functions_1.data,
-                                          example_functions_2.data)
+        functions_1=FunctionData(function_string_1, use_string=True)
+        functions_2=FunctionData(function_string_2, use_string=True)
+        estimated_correlation=correlation(functions_1.data,
+                                          functions_2.data)
         #self.assertEqual(estimated_correlation,0.9452853)
         self.assertAlmostEqual(estimated_correlation,0.9452853,places=7)
 
     def test_mean_function_over_segments(self):
-        mean_function=0
-        self.assertEqual(mean_function,13.5)
+        segments_string_1=""" 1 2
+                            3 6"""
+        segments_1=SegmentData(segments_string_1, use_string=True)
+        function_string_2="""10.5
+11.5
+12.0
+13.0
+13.5
+15.0
+14.0"""
+        functions_2=FunctionData(function_string_2, use_string=True)
+        estimated_mean_function=mean_function(segments_1.data, functions_2.data)
+        self.assertAlmostEqual(estimated_mean_function,13.5,places=1)
 
     def test_average(self):
         values=range(15,19)
-        self.assertEqual(average(values),16.5)
+        self.assertAlmostEqual(average(values),16.5,places=1)
         #print(values)
         #print(average(values),("?=16.5"))
 
